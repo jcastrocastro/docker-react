10f2d625bbef7f0ce59417145efc793b3eba967c
Raul Castro <rcastro@xanadu.ie>
2019-04-13 09:08:35 +0100

    add at least one test

--- src/App.test.js
+++ src/App.test.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-/* it('renders without crashing0', () => {
+it('renders without crashing0', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
-}); */
+});
 
 
